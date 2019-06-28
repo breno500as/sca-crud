6fefd03295c7097d5027169f18793a5ffaabacfb
breno500as <breno500as@gmail.com>
2019-06-27 23:51:48 -0300

    repositorio

--- src/main/java/com/puc/sca/repository/TipoDescricaoInsumoRepository.java
+++ src/main/java/com/puc/sca/repository/TipoDescricaoInsumoRepository.java
@@ -2,10 +2,21 @@ package com.puc.sca.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.puc.sca.entity.insumo.tipo.TipoDescricaoInsumo;
 
 @RepositoryRestResource(collectionResourceRel = "tipos-descricao-insumo", path = "tipos-descricao-insumo")
 public interface TipoDescricaoInsumoRepository extends CrudRepository<TipoDescricaoInsumo, Long> {
 
+	@Override
+	@RestResource(exported = false)
+	default <S extends TipoDescricaoInsumo> S save(S entity) {
+		return null;
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteById(Long id) {
+	}
 }
--- src/main/java/com/puc/sca/repository/TipoMarcaModeloRepository.java
+++ src/main/java/com/puc/sca/repository/TipoMarcaModeloRepository.java
@@ -2,10 +2,21 @@ package com.puc.sca.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.puc.sca.entity.insumo.tipo.TipoMarcaModelo;
 
 @RepositoryRestResource(collectionResourceRel = "tipos-marca-modelo", path = "tipos-marca-modelo")
 public interface TipoMarcaModeloRepository extends CrudRepository<TipoMarcaModelo, Long> {
 
+	@Override
+	@RestResource(exported = false)
+	default <S extends TipoMarcaModelo> S save(S entity) {
+		return null;
+	}
+
+	@Override
+	@RestResource(exported = false)
+	default void deleteById(Long id) {
+	}
 }

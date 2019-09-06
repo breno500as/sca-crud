bc9b32bc54f8e444933ea07ffabd5821ea5045b9
breno500as <breno500as@gmail.com>
2019-09-05 21:45:48 -0300

    eureka

--- src/main/java/com/puc/sca/crud/ScaCrudApplication.java
+++ src/main/java/com/puc/sca/crud/ScaCrudApplication.java
@@ -3,10 +3,10 @@ package com.puc.sca.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class ScaCrudApplication {
 

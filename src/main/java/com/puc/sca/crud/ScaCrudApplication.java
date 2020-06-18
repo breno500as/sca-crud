b459b9510c4369cbf2263eb2f5d8afd3030e0fc3
breno500as <breno500as@gmail.com>
2020-06-18 19:26:48 -0300

    eureka

--- src/main/java/com/puc/sca/crud/ScaCrudApplication.java
+++ src/main/java/com/puc/sca/crud/ScaCrudApplication.java
@@ -3,6 +3,7 @@ package com.puc.sca.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 /**
@@ -47,7 +48,7 @@ import org.springframework.cache.annotation.EnableCaching;
  */
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 //@EnableCircuitBreaker
 @EnableCaching
 public class ScaCrudApplication {

0922cc45358a47dc2af8d5b39de7dbf7d95c6325
breno500as <breno500as@gmail.com>
2020-06-20 00:59:57 -0300

    logs, volumes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11-jdk-slim
 VOLUME /tmp
 COPY target/sca-crud-exec.jar app.jar
-ENTRYPOINT ["java", "$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom", "-jar", "app.jar"]
--- src/main/java/com/puc/sca/crud/controller/TipoInsumoController.java
+++ src/main/java/com/puc/sca/crud/controller/TipoInsumoController.java
@@ -5,6 +5,8 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.puc.sca.crud.entity.insumo.TipoInsumo;
 import com.puc.sca.crud.repository.TipoInsumoRepository;
@@ -12,14 +14,16 @@ import com.puc.sca.crud.repository.TipoInsumoRepository;
 @RestController
 @RequestMapping("tipos-insumo")
 public class TipoInsumoController {
-	
-   @Autowired 
-   private TipoInsumoRepository repository;
 
+	private static final Logger LOG = LoggerFactory.getLogger(TipoInsumoController.class);
+
+	@Autowired
+	private TipoInsumoRepository repository;
 
 	@GetMapping
 	@Cacheable("tipos-insumo")
 	public Iterable<TipoInsumo> findAll() {
+		TipoInsumoController.LOG.info("tipos insumo controller: {}", this.repository);
 		return this.repository.findAll();
 	}
 

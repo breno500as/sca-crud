d1084a3bcd483660b62ea2e004346f5ce49b51e1
breno500as <breno500as@gmail.com>
2020-07-05 22:31:40 -0300

    response entity

--- src/main/java/com/puc/sca/crud/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/puc/sca/crud/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,7 +15,6 @@ import com.puc.sca.util.exception.ExceptionResponse;
 
 
 @ControllerAdvice
-@RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
@@ -24,8 +22,8 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		final ExceptionResponse exceptionResponse = 
 				new ExceptionResponse(
 						new Date(),
-						ex.getMessage(),
-						request.getDescription(false));
+						request.getDescription(false),
+						ex.getMessage());
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
@@ -34,8 +32,8 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		final ExceptionResponse exceptionResponse = 
 				new ExceptionResponse(
 						new Date(),
-						ex.getMessage(),
-						request.getDescription(false));
+						request.getDescription(false),
+						ex.getMessage());
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 
